a4c6639fd3ea39dde3af697f6e85d5b4bc79dc76
Alexander S <70532114+autumnjava@users.noreply.github.com>
2025-01-17 13:29:25 +0100

    changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,15 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      body: ['Inter', 'system-ui', 'sans-serif', 'Arial'],
+    },
     extend: {
       colors: {
         darkGreenGray: '#283618',
         lightGrayGreen: '#B7B7A4',
         lightGreen: '#88b779',
-        accentGreen: '#7edc90'
+        accentGreen: '#7edc90',
       },
     },
   },
